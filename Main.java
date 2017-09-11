aa593c404a3cfc7d8037a79f688c3019e05be353
dprasannalakshmi <31821901+dprasannalakshmi@users.noreply.github.com>
2017-09-11 01:20:19 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+	
+               int array[] = new int[50];
+       Scanner scan = new Scanner(System.in);
+       StudentGroup f=new StudentGroup();
+       f.remove(20);
+	   }//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
